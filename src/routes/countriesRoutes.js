0d5b66dd250bcfe4249d4c5f3e8e09605e8fb79e
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-29 19:07:30 -0300

    Importar y agregar middlewares de validacion, y manejo de errores en las rutas para prueba

--- src/routes/countriesRoutes.js
+++ src/routes/countriesRoutes.js
@@ -11,6 +11,9 @@ import {
 	putPais,
 } from '../controllers/countriesController.js';
 
+import validarPaises from '../middlewares/validations/countryRulesValidations.js';
+import manejarErroresValidacion from '../middlewares/validations/handleValidationsErrors.js';
+
 countriesRouter.get('/', getTodosLosPaises);
 
 // Ruta para obtener el documento pora formulario
@@ -19,9 +22,9 @@ countriesRouter.get('/formData', getDatosFormulario);
 // Buscar país por id
 countriesRouter.get('/:id/buscar', getPaisPorId);
 
-countriesRouter.post('/crear', postPais);
+countriesRouter.post('/crear', validarPaises, manejarErroresValidacion, postPais);
 
-countriesRouter.put('/:id/editar', putPais);
+countriesRouter.put('/:id/editar', validarPaises, manejarErroresValidacion, putPais);
 
 countriesRouter.delete('/:id/eliminar', deletePais);
 
