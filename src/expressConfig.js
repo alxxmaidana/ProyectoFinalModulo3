7b6336600a18fb765c3a4150d1731d3add9f0e12
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-29 19:49:37 -0300

    Instalar y configurar ejs y express-ejs-layouts

--- src/expressConfig.js
+++ src/expressConfig.js
@@ -1,6 +1,7 @@
-// Configurción de express, middlewares, y montar enrutadores
+import path from 'node:path';
 import dotenv from 'dotenv';
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
 import methodOverride from 'method-override';
 import countriesRouter from './routes/countriesRoutes.js';
 
@@ -9,10 +10,17 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configurar motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+app.set('layout', './views/layouts/layout');
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.resolve('./views/public'))); // Servir cualquier archivo estático dentro de public
+app.use(expressLayouts);
 
 // Redireccionar a '/paises' que es donde tengo las rutas
 app.get('/', (_req, res) => {
