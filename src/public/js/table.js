b3efde9acc7781f41d2f25ca2fd92a59b3167a8a
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-06-20 20:47:27 -0300

    Crear filas desplegables contenido extra para cada país

---
+++ src/public/js/table.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const filas = document.querySelectorAll('.rowmain');
+
+  filas.forEach((fila) => {
+    fila.addEventListener('click', () => {
+      const targetId = fila.getAttribute('data-target');
+      const detail = document.getElementById(targetId);
+      const arrowId = targetId.replace('det-', 'arrow-');
+      const arrow = document.getElementById(arrowId);
+
+      detail.classList.toggle('open');
+      arrow.classList.toggle('arrow-open');
+    })
+  })
+})
